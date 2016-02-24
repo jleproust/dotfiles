82efe815ffb60de9e08cc4d6706bf9405f343fa9
Julien Leproust <julien.leproust@opendatasoft.com>
2016-02-24 13:49:37 +0100

    Better git logs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -85,7 +85,8 @@ fi
 alias ga='git add'
 alias gp='git push'
 alias gl='git log'
-alias gpl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gpl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gpl2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
 alias gs='git status'
 alias gd='git diff'
 alias gm='git commit -m'
