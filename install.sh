b629923fad1216dc7986f92c5621fcd458785654
Julien Leproust <julien.leproust@opendatasoft.com>
2016-03-04 19:55:46 +0100

    Mac compatibility, tmux conf, htoprc

--- install.sh
+++ install.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
-DIR=$(dirname $(realpath -s "$0"))
+if [[ $(uname) != 'Darwin' ]]
+then
+    DIR=$(dirname $(realpath -s "$0"))
+else
+    DIR=$(dirname $(realpath "$0"))
+fi
 
-STUFF="zshrc zsh vimrc vim"
-
+STUFF="zshrc zsh vimrc vim tmux.conf"
 for i in $STUFF
 do
     ln -sfn "$DIR/$i" "$HOME/.$i"
 done
 
-VIM_DIRS="undo swaps views"
-for i in $VIM_DIRS
-do
-    mkdir -p "$DIR/vim/$i"
-    chmod go-rwx "$DIR/vim/$i"
-done
+if command -v htop > /dev/null
+then
+    mkdir -p "$HOME/.config/htop"
+    ln -sfn "$DIR/htoprc" "$HOME/.config/htop/htoprc"
+fi
+
+if command -v nvim > /dev/null
+then
+    NVIM_CONFIG="$HOME/.config/nvim"
+    mkdir -p "$NVIM_CONFIG"
+    ln -sfn "$DIR/vimrc" "$NVIM_CONFIG/init.vim"
+    ln -sfn "$DIR/vim/autoload" "$NVIM_CONFIG/autoload"
+    ln -sfn "$DIR/vim/bundle" "$NVIM_CONFIG/bundle"
+fi
+
